108a49f16cf3982d3e7058f53ced7169f8953a9d
denis677 <thedenis.yaaa@gmail.com>
2022-05-06 12:25:30 +0300

    commitchetirnadsat

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,44 @@ ButtonClose.addEventListener('click', closePopup);
 buttonSave.onclick = () => {
   closePopup();
 };
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }];
+
+let profileButton = document.querySelector('.profile__button');
+let popupForm = document.querySelector('.popup-form');
+let ButtonCloseForm = document.querySelector('.popup-form__close');
+
+
+function popupFormOpen () {
+  popupForm.classList.toggle('popup-form_opened');
+};
+
+
+profileButton.addEventListener('click', popupFormOpen);
+ButtonCloseForm.addEventListener('click', popupFormOpen);
+
